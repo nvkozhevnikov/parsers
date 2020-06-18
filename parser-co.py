cc605f260d013994e4e757365a256d910018de41
Nick Kozhevnikov <info@tocomm.ru>
2020-06-19 00:30:06 +0300

    1 version parsers

---
+++ parser-co.py
@@ -0,0 +1,75 @@
+import aiohttp
+import asyncio
+from lxml import html
+from datetime import datetime
+import time
+
+start_time = datetime.now()
+
+
+url = 'https://ultramedstom.ru/'
+scaned_urls = set()
+
+async def worker(q):
+    async with aiohttp.ClientSession() as session:
+        while q.qsize() > 0:
+            u = await q.get()
+            try:
+                async with session.get(u) as response:
+                    code = await response.text()
+
+                tree = html.fromstring(code)
+                tree.make_links_absolute(url)
+                links = tree.xpath('//a/@href')
+                links = set(links)
+
+                title = tree.xpath('//title/text()')[0]
+
+                print (title)
+
+                with open ('t-co.csv', 'a') as f:
+                	f.write(f'{u}\t{title}\n')
+
+                for link in links:
+                    if not link.startswith(url):
+                        continue
+                    elif link in scaned_urls:
+                        continue
+                    else:
+                        scaned_urls.add(link)
+                        await q.put(link)
+            
+            except Exception as e:
+                print(type(e), e)
+
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url)
+        code = await response.text()
+    tree = html.fromstring(code)
+    tree.make_links_absolute(url)
+    links = tree.xpath('//a/@href')
+    links = set(links)
+
+    qu = asyncio.Queue()
+
+    for link in links:
+    	if link.startswith(url):
+    		scaned_urls.add(link)
+    		await qu.put(link)
+    #print(qu.qsize())
+
+    tasks = []
+    for _ in range(100):
+        task = asyncio.Task(worker(qu))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
+print(datetime.now() - start_time)
+	
--- parser-threads.py
+++ parser-threads.py
@@ -3,12 +3,18 @@ from queue import Queue
 from requests_html import HTMLSession
 from concurrent.futures import ThreadPoolExecutor
 from reppy.robots import Robots
+from datetime import datetime
+import time
 
-DOMAIN = 'sterbrust.com'
+start_time = datetime.now()
+
+
+
+DOMAIN = 'ultramedstom.ru'
 scaned_urls = set()
 locker = Lock()
 
-robots = Robots.fetch(f'https://{DOMAIN}/robots.txt')
+#robots = Robots.fetch(f'https://{DOMAIN}/robots.txt')
 
 def worker(queue):
     session = HTMLSession()
@@ -24,9 +30,9 @@ def worker(queue):
             title = resp.html.xpath('//title/text()')[0].strip()
             #print(title)
             if DOMAIN in url:
-                #with locker:
-                #    with open ('t.csv', 'a') as f:
-                #        f.write(f'{url}\t{title}\n')
+                with locker:
+                    with open ('t-th.csv', 'a') as f:
+                       f.write(f'{url}\t{title}\n')
                 print(title)
           
             for link in resp.html.absolute_links: 
@@ -34,11 +40,16 @@ def worker(queue):
                     continue
                 elif DOMAIN not in link:
                     continue
+                # else:
+                # 	if robots.allowed(link, '*'):
+                # 		queue.put(link)
+                # 		with locker:
+                # 			scaned_urls.add(link)
                 else:
-                	if robots.allowed(link, '*'):
-                		queue.put(link)
-                		with locker:
-                			scaned_urls.add(link)
+                    queue.put(link)
+                    with locker:
+                        scaned_urls.add(link)
+
         except Exception as e:
             print(type(e), e)
             
@@ -50,16 +61,23 @@ def main():
     session = HTMLSession()
     resp = session.get(url)
 
-    for link in resp.html.absolute_links:
-    	#print(robots.allowed(link, '*'), link)
-    	if robots.allowed(link, '*'):
-    		qu.put(link)
-    		scaned_urls.add(link)
+    # for link in resp.html.absolute_links:
+    # 	print(robots.allowed(link, '*'), link)
+    # 	if robots.allowed(link, '*'):
+    # 		qu.put(link)
+    # 		scaned_urls.add(link)
     #print('Размер очереди:', qu.qsize())
     #print(link)
+
+    for link in resp.html.absolute_links:
+        qu.put(link)
+        scaned_urls.add(link)
+
    
-    with ThreadPoolExecutor(max_workers=10000) as ex:
-        for _ in range(10000):
+    with ThreadPoolExecutor(max_workers=20) as ex:
+        for _ in range(20):
             ex.submit (worker, qu)
-main()   
+main() 
+
+print(datetime.now() - start_time)  
 
